3b52e57aab861a0c8012c0026429dfa61affbf18
Fraser Benjamin <fraser.benjamin@outlook.com>
2025-02-03 00:04:38 +0000

    Removed console.log

--- src/resolvers/Post.ts
+++ src/resolvers/Post.ts
@@ -4,7 +4,6 @@ export const Post: Resolvers["Post"] = {
   id: (parent) => parent.id,
   message: (parent) => parent.message,
   user: (parent, _, { dataSources }) => {
-    console.log('parent', parent);
     return dataSources.usersService.getUserById(parent.userId);
   },
 }
